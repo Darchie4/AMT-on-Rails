a4442e657bf4c4cf3faa0b655d7e58a1aaed83c1
Darchie4 <mads456dk@gmail.com>
2024-02-22 15:54:21 +0100

    Fixed broken migrations

--- db/migrate/20240215123401_create_lessons.rb
+++ db/migrate/20240215123401_create_lessons.rb
@@ -2,8 +2,7 @@ class CreateLessons < ActiveRecord::Migration[7.1]
   def change
     create_table :lessons do |t|
       t.string :name
-      t.string :short_description
-      t.text :full_description
+      t.text :description
       t.date :season_start
       t.date :season_end
       t.integer :min_age
--- db/migrate/20240215123750_create_lesson_time_locations.rb
+++ db/migrate/20240215123750_create_lesson_time_locations.rb
@@ -1,8 +1,8 @@
 class CreateLessonTimeLocations < ActiveRecord::Migration[7.1]
   def change
     create_table :lesson_time_locations do |t|
-      t.integer.foreign_key :lessons
-      t.integer.foreign_key :locations
+      t.integer :lesson_id
+      t.integer :location_id
       t.string :week_day
       t.time :start_time
       t.time :end_time
