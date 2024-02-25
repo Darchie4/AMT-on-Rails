c1c4f3eaf607f095a359c0bc7f8ddf173033a9fc
Katrine Birk <katrinebirk2@live.dk>
2024-02-25 14:34:50 +0100

    user migration update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_25_095453) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_25_133123) do
   create_table "addresses", force: :cascade do |t|
     t.string "country"
     t.string "address"
@@ -80,6 +80,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_25_095453) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.date "birthday"
+    t.integer "address_id"
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
