376c69317cea059015fd911a404978afecd74643
Darchie4 WSL <mads456dk@gmail.com>
2024-02-25 16:54:51 +0100

    Precompile may work now

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,9 @@
+// This is the main entry point for your application
+
+// Import your main application JavaScript file
+import '../src/main.js';
+
+// You can also import other JavaScript modules or files as needed
+// import './otherModule';
+
+// Any additional initialization code or setup can go here
